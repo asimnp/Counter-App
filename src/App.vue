4badc4091a4a42dbb45eaf439a83152d86a14585
Asim Shrestha <asimshrestha608@gmail.com>
2022-03-15 18:23:07 +0545

    feat: increment, decrement & reset counter

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,9 @@
 <template>
   <h1>Counter App</h1>
   <h3>Counter: {{ counter }}</h3>
+  <button @click="increment">Increment</button>
+  <button @click="decrement">Decrement</button>
+  <button @click="reset">Reset</button>
 </template>
 
 <script>
@@ -9,6 +12,17 @@ export default {
     return {
       counter: 0,
     };
+  },
+  methods: {
+    increment() {
+      this.counter += 1;
+    },
+    decrement() {
+      this.counter -= 1;
+    },
+    reset() {
+      this.counter = 0;
+    },
   },
 };
 </script>
